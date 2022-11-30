41b1b763dbeb7a67aafedd2d4ce41e2c4832aaaf
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-11-29 16:24:48 -0800

    first commit

---
+++ elist.s
@@ -0,0 +1,8 @@
+#  Name:   Nguyen, Peter
+#  Project:  #4
+#  Due:        12/01/2022 
+#  Course:  cs-2640-04-f22 
+# 
+#  Description: 
+#    Creates a linked list as well as a file and uses it to store information and traverses it.
+ptfname:  .asciiz   “path/enames.dat" 
